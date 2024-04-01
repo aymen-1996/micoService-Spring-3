c4ca115c583d7184259d163dfe9aba2401d2a2e5
Chouaibi Mohamed Aymen <chouaibiaymen03@gmail.com>
2024-04-01 12:18:25 +0000

    microService SpringBoot 3

---
+++ discovery/src/test/java/com/chouaibi/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chouaibi.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ school/src/test/java/com/chouaibi/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chouaibi.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
